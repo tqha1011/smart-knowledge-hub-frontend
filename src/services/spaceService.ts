f5a98d600342b6fcbc40dd2b7a414655bf4446d8
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-29 17:56:39 +0700

    feat: implement 2 method create & get space type

--- src/services/spaceService.ts
+++ src/services/spaceService.ts
@@ -1,11 +1,15 @@
-import type { RequestSpaceDto } from "../types/commonType/space";
 import { handleApiError } from "../shared/handleApiError";
+import type {
+  CreateSpaceTypeDto,
+  RequestSpaceDto,
+} from "../types/commonType/space";
 import api from "./api";
 
+const alias = "knowledge-spaces";
 export const knowledgeSpaceService = {
   createSpace: async (newSpace: RequestSpaceDto) => {
     try {
-      const response = await api.post("/knowledge-spaces", {
+      const response = await api.post(`/${alias}`, {
         name: newSpace.name,
         description: newSpace.description,
         typePublicId: newSpace.typePublicId,
@@ -18,7 +22,7 @@ export const knowledgeSpaceService = {
 
   updateSpace: async (publicId: string, updateSpace: RequestSpaceDto) => {
     try {
-      const response = await api.put(`/knowledge-spaces/${publicId}`, {
+      const response = await api.put(`/${alias}/${publicId}`, {
         name: updateSpace.name,
         description: updateSpace.description,
         typePublicId: updateSpace.typePublicId,
@@ -31,7 +35,29 @@ export const knowledgeSpaceService = {
 
   getUserRole: async (spacePublicId: string) => {
     try {
-      const response = await api.get(`knowledge-spaces/${spacePublicId}/role`);
+      const response = await api.get(`/${alias}/${spacePublicId}/role`);
+      return response.data;
+    } catch (error) {
+      throw handleApiError(error);
+    }
+  },
+};
+
+export const knowledgeSpaceTypeService = {
+  createType: async (newType: CreateSpaceTypeDto) => {
+    try {
+      const response = await api.post(`/${alias}/types`, {
+        name: newType.name,
+      });
+      return response.data;
+    } catch (error) {
+      throw handleApiError(error);
+    }
+  },
+
+  getListTypes: async () => {
+    try {
+      const response = await api.get(`/${alias}/types`);
       return response.data;
     } catch (error) {
       throw handleApiError(error);
