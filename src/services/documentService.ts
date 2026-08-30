8b326b8945c47e012f5c21d280c83ccacffaff72
Tran Quang Ha <tranquangha2006@gmail.com>
2026-08-30 13:40:14 +0700

    feat: implement method get list document for user

--- src/services/documentService.ts
+++ src/services/documentService.ts
@@ -1,1 +1,19 @@
-export const documentService = {};
+import { handleApiError } from "../shared/handleApiError";
+import type { PaginationResponse } from "../types/commonType/pagination";
+import type { DocumentListItemDto } from "../types/documentType";
+import api from "./api";
+
+const firstAlias = "knowledge-spaces";
+const afterAlias = "documents";
+export const documentService = {
+  getListDocuments: async (spacePublicId: string) => {
+    try {
+      const response = await api.get<PaginationResponse<DocumentListItemDto>>(
+        `${firstAlias}/${spacePublicId}/${afterAlias}`,
+      );
+      return response.data;
+    } catch (error) {
+      throw handleApiError(error);
+    }
+  },
+};
